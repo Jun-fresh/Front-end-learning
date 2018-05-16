ec2a1e21542a6ce158468b0c1695fd075eee2e15
Jun-fresh <837691644@qq.com>
2018-05-16 17:22:32 +0800

    add HTML code

---
+++ html/action.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<title>Document</title>
+</head>
+<body>
+	这是一个服务器文件
+</body>
+</html>
